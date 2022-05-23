85940ec999a13043c360772103e66d79b614c76b
Samuel Drewes <samuel.drewes@gmail.com>
2022-05-23 14:32:24 +0200

    correction

--- my_first_package/lib.py
+++ my_first_package/lib.py
@@ -2,9 +2,9 @@ from random import randint
 
 def try_me():
     x = randint(1,3)
-    if x = 1:
+    if x == 1:
         return 'oh yeah'
-    if x = 2:
+    if x == 2:
         return 'mhmmm'
-    if x = 3:
+    if x == 3:
         return 'allesss gut'

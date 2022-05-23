1093f5112f0c10e35be0c6089b4525299a504387
Samuel Drewes <samuel.drewes@gmail.com>
2022-05-23 14:30:32 +0200

    package

---
+++ my_first_package/lib.py
@@ -0,0 +1,10 @@
+from random import randint
+
+def try_me():
+    x = randint(1,3)
+    if x = 1:
+        return 'oh yeah'
+    if x = 2:
+        return 'mhmmm'
+    if x = 3:
+        return 'allesss gut'
